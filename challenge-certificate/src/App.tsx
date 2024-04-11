f29ec9e4f7c815a93a307582993edc78c0f1c21f
Vitor Soares <vitorsoaresf@gmail.com>
2024-04-11 13:49:56 -0300

    chore: build project

---
+++ challenge-certificate/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Buscar certificado</h1>
+      <input />
+    </>
+  );
+}
+
+export default App;
